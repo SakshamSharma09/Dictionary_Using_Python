a6ca79b1ca745b30f4139bd520ce50011fb318f2
SakshamSharma09 <84015186+SakshamSharma09@users.noreply.github.com>
2021-09-10 11:42:00 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -2,21 +2,29 @@ import json
 from difflib import get_close_matches as gcm
 
 def diction(dic_word):
+    
     if(dic_word in file):
         return file[dic_word]
+    
     elif(dic_word.title() in file):
         return file[dic_word.title()]
+    
     elif(dic_word.upper() in file):
         return file[dic_word.upper()]
+    
     elif len(gcm(dic_word , file.keys()))>0:
         print("Did you mean %s instead" %gcm(dic_word,file.keys())[0])
         decide = input("Y for yes and N for no")
+        
         if decide.lower() == 'y':
             return file[gcm(dic_word,file.keys())[0]]
+        
         elif decide.lower() == 'n':
             return ("Word Not Found!")
+        
         else:
             return("Wrong input Enter Again")
+        
     else:
         return ("Word Not Found!")
 
